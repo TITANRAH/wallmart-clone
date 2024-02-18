4eaad98bf065a80762cc3a780d0c5f58748c531d
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-02-18 02:00:57 -0300

    desde mac

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <h1>Lets build wallmart clone</h1>
+      <h1>Lets build wallmart hehehe</h1>
     </main>
   );
 }
